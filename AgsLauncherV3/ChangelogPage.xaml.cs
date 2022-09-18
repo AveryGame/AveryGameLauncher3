5d641d8f7d42152e36bc01d986172d2764e7bedf
axi <76423997+AyeItsAxi@users.noreply.github.com>
2022-09-17 22:14:35 -0700

    animations and stuff i think

--- AgsLauncherV3/ChangelogPage.xaml.cs
+++ AgsLauncherV3/ChangelogPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,11 +24,45 @@ namespace AgsLauncherV3
             HomePage mw = new HomePage();
             InitializeComponent();
             this.ShowsNavigationUI = false;
-            mw.Content = new UserControl();
+            var b = contentHost;
+            var fade = new DoubleAnimation()
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.15),
+            };
+            Storyboard.SetTarget(fade, b);
+            Storyboard.SetTargetProperty(fade, new PropertyPath(Button.OpacityProperty));
+            var sb = new Storyboard();
+            sb.Children.Add(fade);
+            sb.Begin();
+            /*ThicknessAnimation contenthostMargin = new ThicknessAnimation();
+            contenthostMargin.From = new Thickness(-900, 0, 900, 0);
+            contenthostMargin.To = new Thickness(0, 0, 0, 0);
+            contenthostMargin.Duration = TimeSpan.FromSeconds(0.15);
+            contenthostMargin.FillBehavior = FillBehavior.HoldEnd;
+            var thicknessAnim = new Storyboard();
+            Storyboard.SetTargetName(contenthostMargin, "contentHost");
+            Storyboard.SetTargetProperty(contenthostMargin, new PropertyPath(Grid.MarginProperty));
+            thicknessAnim.Children.Add(contenthostMargin);
+            thicknessAnim.Begin(this);
+            thicknessAnim.Children.Remove(contenthostMargin);*/
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var b = contentHost;
+            var fade = new DoubleAnimation()
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.15),
+            };
+            Storyboard.SetTarget(fade, b);
+            Storyboard.SetTargetProperty(fade, new PropertyPath(Button.OpacityProperty));
+            var sb = new Storyboard();
+            sb.Children.Add(fade);
+            sb.Begin();
             HomePage hp = new HomePage();
             this.ShowsNavigationUI = false;
             MainWindow bootStrapper = new MainWindow();
--- AgsLauncherV3/HomePage.xaml.cs
+++ AgsLauncherV3/HomePage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using static System.Threading.Tasks.Task;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -10,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using XamlAnimatedGif;
 
 namespace AgsLauncherV3
 {
@@ -22,17 +25,72 @@ namespace AgsLauncherV3
         {
             InitializeComponent();
             this.ShowsNavigationUI = false;
+            var b = contentHost;
+            var fade = new DoubleAnimation()
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.15),
+            };
+            Storyboard.SetTarget(fade, b);
+            Storyboard.SetTargetProperty(fade, new PropertyPath(Button.OpacityProperty));
+            var sb = new Storyboard();
+            sb.Children.Add(fade);
+            sb.Begin();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            var b = contentHost;
+            var fade = new DoubleAnimation()
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.15),
+            };
+            Storyboard.SetTarget(fade, b);
+            Storyboard.SetTargetProperty(fade, new PropertyPath(Button.OpacityProperty));
+            var sb = new Storyboard();
+            sb.Children.Add(fade);
+            sb.Begin();
+            /*ThicknessAnimation contenthostMargin = new ThicknessAnimation();
+            contenthostMargin.From = new Thickness(0, 0, 0, 0);
+            contenthostMargin.To = new Thickness(900, 0, -900, 0);
+            contenthostMargin.Duration = TimeSpan.FromSeconds(0.15);
+            contenthostMargin.FillBehavior = FillBehavior.HoldEnd;
+            var thicknessAnim = new Storyboard();
+            Storyboard.SetTargetName(contenthostMargin, "contentHost");
+            Storyboard.SetTargetProperty(contenthostMargin, new PropertyPath(Grid.MarginProperty));
+            thicknessAnim.Children.Add(contenthostMargin);
+            thicknessAnim.Begin(this);
+            thicknessAnim.Children.Remove(contenthostMargin);*/
             ChangelogPage clp = new ChangelogPage();
             this.ShowsNavigationUI = false;
             MainWindow bootStrapper = new MainWindow();
             this.RemoveLogicalChild(this.wowz2);
+            Console.WriteLine("Removed logical child - calling new page.");
             bootStrapper.RemoveLogicalChildOnWindowLoad();
+            await Delay(150);
             this.NavigationService.Navigate(clp);
-            
+        }
+
+        private async void windowChange_MouseEnter(object sender, MouseEventArgs e)
+        {
+            int width = 103;
+            int height = 21;
+            for (int i = 0; i > 500; i++)
+            {
+                windowChange.Width = width;
+                windowChange.Height = height;
+                width = width + 3;
+                height = height + 1;
+                await Delay(25);
+            }
+        }
+
+        private void windowChange_MouseLeave(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
